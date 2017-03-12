8c5e37ed01963a8b16b70482b6c323d9b2ab95a6
MathiasB <git@denbeke.be>
2017-03-12 23:49:19 +0100

    WIP: Authentication with JWT

---
+++ app/src/Auth.ts
@@ -0,0 +1,62 @@
+import {router} from './main'
+
+export default {
+
+    // authentication status
+    authenticated: false,
+
+    // Send a request to the login URL and save the returned JWT
+
+    login(context, creds, redirect) {
+        /*
+        context.$http.post('/login', creds, (data) => {
+            localStorage.setItem('user', JSON.stringify(data))
+
+            this.authenticated = true
+            context.$root.user = data
+
+            // Redirect to a specified route
+            if (redirect) {
+                router.go(redirect)
+            }
+
+            console.log("post");
+
+            }).error((errors) => {
+                context.errors = errors;
+            })
+            */
+        context.$http.post('/login', creds).then(response => {
+        
+                // get body data
+                localStorage.setItem('user', JSON.stringify(response.data))
+                console.log(response.data)
+            
+                
+                this.authenticated = true
+                context.$root.user = response.data
+                
+                // Redirect to a specified route
+                alert(redirect);
+                if (redirect) {
+                    console.log(redirect);
+                    router.push(redirect)
+                }
+                
+
+        
+          }, response => {
+            // error callback
+            console.log("error");
+            console.log(response);
+          });
+
+    },
+
+    // To log out
+    logout: function() {
+        localStorage.removeItem('user');
+        this.authenticated = false;
+        router.go('/login')
+    }
+}
--- app/src/main.ts
+++ app/src/main.ts
@@ -3,12 +3,18 @@
 //import {test} from './test';
 import * as Vue from 'vue';
 import * as Router from 'vue-router';
+import * as VueResource from 'vue-resource';
+Vue.use(VueResource)
 Vue.use(Router);
+
+// authentication service
+import Auth from './Auth';
 
 import * as AudioPlayerComponent from './components/AudioPlayer.vue';
 import * as AlbumsComponent from './components/Albums.vue';
 import * as AlbumComponent from './components/Album.vue';
 import * as CurrentQueueComponent from './components/CurrentQueue.vue';
+import * as LoginComponent from './components/Login.vue';
 
 
 import './AudioPlayer';
@@ -22,15 +28,33 @@ PubSub.subscribe(AudioPlayerEvents.SongChanged, (song: Song) => {
     Notifier.notify('Playing song: ' + song.name);
 });
 
-var router = new Router({
+
+export var testAuth = Auth;
+
+export var router = new Router({
     routes: [
-          { path: '/', component: AlbumsComponent },
-          { path: '/albums', component: AlbumsComponent },
-          { path: '/albums/:id', component: AlbumComponent },
-          { path: '/current-queue', component: CurrentQueueComponent }
+          { path: '/', component: AlbumsComponent, meta: { requiresAuth: true } },
+          { path: '/albums', component: AlbumsComponent, meta: { requiresAuth: true } },
+          { path: '/albums/:id', component: AlbumComponent, meta: { requiresAuth: true } },
+          { path: '/current-queue', component: CurrentQueueComponent, meta: { requiresAuth: true } },
+          { path: '/login', component: LoginComponent }
 
     ],
 });
+
+
+router.beforeEach(function (to, from, next) {
+    console.log(to);
+    console.log(testAuth);
+    if (to.meta.requiresAuth && !testAuth.authenticated) {
+        // if route requires auth and user isn't authenticated
+        console.log("Not logged in");
+        next('/login')
+    } else {
+        console.log("Logged in");
+        next()
+    }
+})
 
 var app = new Vue({
     el: '#app',
@@ -40,6 +64,36 @@ var app = new Vue({
         'albums': AlbumsComponent,
         'album': AlbumComponent,
         'current-queue': CurrentQueueComponent,
+    },
+    data: function() {
+        return { user: {} };
+    },
+    computed: {
+        auth: function() {
+            return testAuth;
+        }
+    },
+    methods: {
+        checkLocalStorage: function() {
+            console.log(localStorage)
+            if (localStorage.user) {
+                console.log(localStorage.user)
+                this.user = JSON.parse(localStorage.user);
+                Vue.http.headers.common['Authorization'] = 'Bearer ' + this.user.token;
+                testAuth.authenticated = true;
+            }
+        },
+        logout: function() {
+            this.user = {};
+            Auth.logout();
+        }
+    },
+    mounted: function() {
+        testAuth.test = "gataap";
+        console.log("mounted");
+        this.checkLocalStorage();
     }
 });
+
+Vue.http.options.emulateJSON = true;
 
