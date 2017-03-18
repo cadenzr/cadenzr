e645af04d891fe177fc94aa29a7b2bcac18af86c
MathiasB <git@denbeke.be>
2017-03-18 15:44:15 +0100

    Split probers into files

--- probers/probers.go
+++ probers/probers.go
@@ -1,23 +1,16 @@
 package probers
 
 import (
-	"bytes"
-	"encoding/json"
 	"errors"
 	"fmt"
-	"io/ioutil"
 	"mime"
 	"net/http"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"regexp"
 	"strconv"
-
-	"github.com/badgerodon/mp3"
+	
 	log "github.com/cadenzr/cadenzr/log"
-	id3 "github.com/mikkyang/id3-go"
-	id3v2 "github.com/mikkyang/id3-go/v2"
 )
 
 type AudioMeta struct {
@@ -101,158 +94,6 @@ type AudioProber interface {
 	ProbeAudio(file string) (*AudioMeta, error)
 
 	fmt.Stringer
-}
-
-type ffprobeAudioProber struct{}
-
-func (f *ffprobeAudioProber) getCover(file string, meta *AudioMeta) {
-	ffmpeg, err := exec.LookPath("ffmpeg")
-	if err != nil {
-		return
-	}
-
-	cmd := exec.Command(ffmpeg,
-		"-i", file,
-		"-f", "image2",
-		"pipe:1",
-	)
-
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		return
-	}
-
-	if err = cmd.Start(); err != nil {
-		return
-	}
-
-	buf, err := ioutil.ReadAll(stdout)
-	if err != nil {
-		return
-	}
-
-	if err = cmd.Wait(); err != nil {
-		return
-	}
-
-	meta.CoverBufer = buf
-}
-
-func (p *ffprobeAudioProber) ProbeAudio(file string) (meta *AudioMeta, err error) {
-	ffprobe, err := exec.LookPath("ffprobe")
-	if err != nil {
-		return
-	}
-
-	cmd := exec.Command(ffprobe,
-		"-print_format", "json",
-		"-show_entries", "format=duration:format_tags",
-		file,
-	)
-
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		return
-	}
-
-	if err = cmd.Start(); err != nil {
-		return
-	}
-
-	response := struct {
-		Format struct {
-			Duration string `json:"duration"`
-			Tags     struct {
-				Title       string `json:"title"`
-				Artist      string `json:"artist"`
-				Album       string `json:"album"`
-				Genre       string `json:"genre"`
-				Track       string `json:"track"`
-				TotalTracks string `json:"totaltracks"`
-				Date        string `json:"date"`
-				AlbumArtist string `json:"album_artist"`
-			}
-		}
-	}{}
-
-	if err = json.NewDecoder(stdout).Decode(&response); err != nil {
-		return
-	}
-
-	if err = cmd.Wait(); err != nil {
-		return
-	}
-
-	meta = &AudioMeta{}
-	meta.Album = response.Format.Tags.Album
-	meta.Title = response.Format.Tags.Title
-	meta.Artist = response.Format.Tags.Artist
-	meta.Genre = response.Format.Tags.Genre
-	meta.Album = response.Format.Tags.Album
-	meta.Track = parseInt(response.Format.Tags.Track)
-	meta.TotalTracks = parseInt(response.Format.Tags.TotalTracks)
-	meta.Year = parseInt(response.Format.Tags.Date)
-	meta.AlbumArtist = response.Format.Tags.AlbumArtist
-	meta.Duration, _ = strconv.ParseFloat(response.Format.Duration, 64)
-
-	p.getCover(file, meta)
-
-	return
-}
-
-func (p *ffprobeAudioProber) hasFFprobe() bool {
-	_, err := exec.LookPath("ffprobe")
-	if err != nil {
-		return false
-	}
-
-	return true
-}
-
-func (p *ffprobeAudioProber) String() string {
-	return "ffprobeAudioProber"
-}
-
-type id3AudioProber struct{}
-
-func (p *id3AudioProber) cleanString(s string) string {
-	// Something with macos?
-	return string(bytes.Trim([]byte(s), "\x00"))
-}
-
-func (p *id3AudioProber) ProbeAudio(file string) (meta *AudioMeta, err error) {
-	id3f, err := id3.Open(file)
-	if err != nil {
-		return
-	}
-	defer id3f.Close()
-
-	meta = &AudioMeta{}
-	meta.Title = p.cleanString(id3f.Title())
-	meta.Year = parseInt(p.cleanString(id3f.Year()))
-	meta.Genre = p.cleanString(id3f.Genre())
-	meta.Artist = p.cleanString(id3f.Artist())
-	meta.Album = p.cleanString(id3f.Album())
-
-	f, err := os.Open(file)
-	if err != nil {
-		return
-	}
-	d, err := mp3.Length(f)
-	if err == nil {
-		defer f.Close()
-		meta.Duration = d.Seconds()
-	}
-
-	if apic := id3f.Frame("APIC"); apic != nil {
-		meta.CoverBufer = apic.(*id3v2.ImageFrame).Data()
-	}
-
-	return
-}
-
-func (p *id3AudioProber) String() string {
-	return "id3Prober"
 }
 
 type prober struct {
